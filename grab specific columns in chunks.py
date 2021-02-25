0e77dc27c3db83c83ec0450c86d20944988be8c4
Lukas Sherman <lsherman@berkeley.edu>
2021-02-25 15:49:11 -0700

    update chunking

--- grab specific columns in chunks.py
+++ grab specific columns in chunks.py
@@ -1,36 +1,34 @@
 
-# Make a new csv
-# For each line of the old CSV:  Take all the 'fields' from the old csv
-# Write to new csv
+# Process a huge CSV
 
 import pandas as pd
 import numpy as np
+from ast import literal_eval
+import pickle
 
 
-fields = np.array([1,2,5,6,7,51,54,57,58,104,136,149,215,274,281,294,295,298])-1 #set of columns that I want from the csv file (-1) is there because I originally tried to do this in R
-
-data_final = []
-
-chunksize = 10 ** 4 #this is just some file size, could be smaller or lower
-
+chunksize = 10 ** 2 ## This sets the number of rows to grab.
 count = 1
-for chunk in pd.read_csv("file.txt", #filename
-                         header=0, encoding="unicode_escape", chunksize=chunksize,low_memory=False):
-    data = chunk.get_chunk(chunksize)[:, fields]
-    data = chunk[chunk.columns[fields]]
-    data_final.append(data) # stack the tables, if the extracted data is still large this will cause issues.
-    #You could add lines here so that you ONLY grab data rows that match your filters. That would dramatically reduce the memory usage.
+
+for chunk in pd.read_csv("google_image_features_cnn.csv", #filename
+                          chunksize=chunksize,low_memory=False):
+    
+    ####
+    ### PROCESS HERE
+    ## EXAMPLE BEGIN
+    chunk["feature_list"] = chunk["features"].apply(literal_eval) # read column as literal
+
+    chunk.pop("features") #drop features column
+    
+    data_final.append(chunk) # stack the tables, if the extracted data is still large this will cause memory issues.
 
     print("Iteration" , count , " complete.")
     count += 1
+    ## EXAMPLE END
+    if i == 1:
+        names = chunk.columns
 
 final_np_array = np.vstack(data_final) #this just stacks the dataframes into a array (basically a matrix)
 
-final_Pandas_DF = pd.DataFrame(final_np_array) #This turns the matrix into a pandas df
-
-correct_column_names = chunk.columns[fields] #make sure we get the column names back
-final_Pandas_DF.columns = [correct_column_names] #set the column names
-
-#pickle.dump(final_Pandas_DF, open( "output_file.p", "wb" ) ) #This is to save the reduced data frame as a pickle (python thing) so you dont have to run this again
-#final_Pandas_DF.to_csv('output_file.csv') #This is to save as a csv so you dont have to run this all again.
-
+final_df = pd.DataFrame(final_np_array) #This turns the matrix into a pandas df
+final_df.columns = names
